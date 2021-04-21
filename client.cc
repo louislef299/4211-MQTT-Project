2d1ed57c4ed9d637900cc4a1c7b6199f357a459f
Louis-Lefebvre <louislefebvre1999@gmail.com>
2021-04-21 01:50:46 -0500

    publish cin isn't working properly, going to try on csecomputers tomorrow

--- client.cc
+++ client.cc
@@ -13,30 +13,23 @@
 
 using namespace std;
 
-enum command{
-  connection,
-  disconnect,
-  publish,
-  subscribe,
-  quit,
-  none
-};
+enum command{CONNECTION,DISCONNECT,PUBLISH,SUBSCRIBE,QUIT,NONE};
 
 command hashit(string& command){
   for(int i=0;i<command.length();i++)
     command.at(i) = toupper(command.at(i));
 
   if("CONNECT" == command)
-    return connection;
+    return CONNECTION;
   if("DISCONNECT" == command)
-    return disconnect;
+    return DISCONNECT;
   if("PUBLISH" == command)
-    return publish;
+    return PUBLISH;
   if("SUBSCRIBE" == command)
-    return subscribe;
+    return SUBSCRIBE;
   if("QUIT" == command)
-    return quit;
-  return none;
+    return QUIT;
+  return NONE;
 }
 
 int make_connection(){
@@ -84,52 +77,56 @@ int make_connection(){
   return sockfd;
 }
 
-int pub(int sockfd){
+int publish_mqtt(int sockfd){
   char commandBuff[200];
   memset(commandBuff, '0',std::strlen(commandBuff));
   
-  string topicNumber;
+  char topicNumber;
   char topic[10];
-  while(1){
+  int loop = 1;
+  while(loop){
     std::cout << "Please choose a topic(type a number):\n[1] Weather\n[2] News\n[3] Health\n[4] Security\n";
     
-    cin >> topicNumber;
-    int topicNum = stoi(topicNumber);
+    std::cin >> topicNumber;
+    int topicNum = topicNumber - '0';
     if(topicNum == 1){
       strcpy(topic,"weather");
-      break;
+      loop=0;
     }
     else if(topicNum == 2){
       strcpy(topic,"news");
-      break;
+      loop=0;
     }
     else if(topicNum == 3){
       strcpy(topic,"health");
-      break;
+      loop=0;
     }
     else if(topicNum == 4){
       strcpy(topic,"security");
-      break;
+      loop=0;
     }
     else
       std::cout << "Invalid response, please try again\n";
   }
 
-  string msg;
+  char msg[120];
   std::cout << "What message would you like to publish?\nPublication(max 100 characters): ";
-  std::getline(std::cin,msg);
+  std::cin.get(msg,120);
+  
   strcpy(commandBuff,"PUB,");
   strcat(commandBuff,topic);
   strcat(commandBuff,",");
   //need to change message to a char*
-  strcat(commandBuff,msg.c_str());
+  strcat(commandBuff,msg);
   write(sockfd,commandBuff,strlen(commandBuff));
   printf("Your sent package: %s\n",commandBuff);
   
   return 0;
-
 }
 
+/****************************************************** 
+    MQTT Project
+ ******************************************************/
 int main(int argc, char *argv[]){
   std::cout << "Welcome to the Project 1 Publishing service!\n";
 
@@ -142,7 +139,7 @@ int main(int argc, char *argv[]){
     cin >> command;
     
     switch(hashit(command)){
-    case connection:
+    case CONNECTION:
       if(sockfd != -1)
 	std::cout << "Already connected!\n";
       else{
@@ -150,14 +147,14 @@ int main(int argc, char *argv[]){
 	std::cout << "Successful Connection\n";
       }
       break;
-    case disconnect:
+    case DISCONNECT:
       break;
-    case publish:
-      pub(sockfd);
+    case PUBLISH:
+      publish_mqtt(sockfd);
       break;
-    case subscribe:
+    case SUBSCRIBE:
       break;
-    case quit:
+    case QUIT:
       cont = 0;
       break;
     default:
